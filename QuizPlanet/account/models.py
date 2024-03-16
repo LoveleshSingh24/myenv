d22cfdc0bc3bdebac270588f36fb9dcccbaa5269
lovelesh24 <lovesinghsisodiya2403@gamil.com>
2024-03-16 20:41:38 +0530

    created migration / DB for registration field

--- QuizPlanet/account/models.py
+++ QuizPlanet/account/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
-    usernmae=models.OneToOneField(User,on_delete=models.CASCADE,null=True) #verbose name
-    email_address=models.CharField(max_lenght=55,unique=True,null=True)
+    user=models.OneToOneField(User,on_delete=models.CASCADE,null=True,verbose_name='User Object') #verbose name
+    email_address=models.CharField(max_length=55,unique=True,null=True,verbose_name='Email')
     bio = models.TextField(blank=True,null=True)
+    profile_img=models.ImageField(upload_to='profile_images',default='user.png',blank=True,null=True,verbose_name='Profile Pic')
+    location=models.CharField(max_length=100,blank=True,null=True)
+    GENDER=(
+        ('Male','Male'),
+        ('Female','Female'),
+        ('Other','Other'),
+    )
+    gender=models.CharField(max_length=6,choices=GENDER,blank=True,null=True)
+    
+    def __str__(self):
+        return self.user.username
