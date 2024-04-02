b5240bafe5a2760844b24b3e6877993a0daf9eef
lovelesh24 <lovesinghsisodiya2403@gamil.com>
2024-04-03 00:47:46 +0530

    added leaderboard functionality

--- QuizPlanet/base/views.py
+++ QuizPlanet/base/views.py
@@ -3,16 +3,23 @@ from django.shortcuts import HttpResponse
 from django.contrib.auth.models import User
 from account.models import Profile
 from django.contrib.auth.decorators import login_required
+from quiz.models import UserRank
 # Create your views here.
 
 def home(request):
-    if(request.user.is_authenticated):
+    leaderboard_users = UserRank.objects.order_by('rank')[:4]
+    if request.user.is_authenticated:
         user_object = User.objects.get(username=request.user)
-        user_profile2 =Profile.objects.get(user=user_object)
-        
-        context={"user_profile2":user_profile2}
-        return render(request,'welcome.html',context)
+        user_profile2 = Profile.objects.get(user=user_object)
+        context = {"user_profile2": user_profile2, "leaderboard_users": leaderboard_users}
     else:
-        context={}
-        return render(request,'welcome.html',context)
-        
+        context = {"leaderboard_users": leaderboard_users}
+    return render(request, 'welcome.html', context)
+
+@login_required(login_url='login')
+def leaderboard_view(request):
+    leaderboard_users = UserRank.objects.order_by('rank')
+    user_object = User.objects.get(username=request.user)
+    user_profile2 = Profile.objects.get(user=user_object)
+    context = {"leaderboard_users": leaderboard_users, "user_profile2": user_profile2}
+    return render(request, "leaderboard.html", context)
--- QuizPlanet/quiz/views.py
+++ QuizPlanet/quiz/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Quiz,Category
 from django.db.models import Q
 from quiz.models import QuizSubmission
-
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 @login_required(login_url='login')
@@ -44,33 +44,32 @@ def search_view(request,category):
     context={"user_profile2":user_profile2,"quizzes":quizzes,"categories":categories}
     return render(request,"all-quiz.html", context)
 
+
 @login_required(login_url='login')
-def quiz_view(request,quiz_id):
+def quiz_view(request, quiz_id):
     user_object = User.objects.get(username=request.user)
-    user_profile2 =Profile.objects.get(user=user_object)
-    
+    user_profile2 = get_object_or_404(Profile, user=user_object)
 
     quiz = Quiz.objects.filter(id=quiz_id).first()
-    total_question=quiz.question_set.all().count()
+
+    total_questions = quiz.question_set.all().count()
+
     if request.method == "POST":
-        #get the score 
-        score = int(request.POST.get('score',0))
+        
+        # Get the score
+        score = int(request.POST.get('score', 0))
 
-        #check if the user has already submitted the quiz
-        if QuizSubmission.objects.filter(user=request.user,quiz=quiz).exists():
-            messages.success(request,f"This time you got {score} out of {total_question}")
-            return redirect('quiz',quiz_id)
-
-        #save the new quiz
-        submission = QuizSubmission(user=request.user,quiz=quiz,score=score)
+        # Check if the user has already submiited the quiz
+        if QuizSubmission.objects.filter(user=request.user, quiz=quiz).exists():
+            messages.success(request, f"This time you got {score} out of {total_questions}")
+            return redirect('quiz', quiz_id)
+        
+        # save the new quiz submission
+        submission = QuizSubmission(user=request.user, quiz=quiz, score=score)
         submission.save()
         
-        messages.success(request,f"Quiz Submitted Successfully. You got {score} out of {total_question}")
-        return redirect('quiz',quiz_id)
+        messages.success(request, f"Quiz Submitted Successfully. You got {score} out of {total_questions}")
+        return redirect('quiz', quiz_id)
 
-    if quiz is not None :
-       context={"user_profile2":user_profile2,"quiz":quiz}
-    else:
-        return redirect('all_quiz')
-    
-    return render(request,'quiz.html',context)
+    context = {"user_profile2": user_profile2, "quiz": quiz}
+    return render(request, 'quiz.html', context)
